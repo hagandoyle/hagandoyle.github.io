6e2163c038cfb57e99c33379a52a8fcb27291a6a
Hagan Doyle <hdoyle2@lsu.edu>
2018-03-08 00:53:56 -0600

    .js 3/8/18
    ok

---
+++ code-assignment-03.js
@@ -0,0 +1,9 @@
+let myNumber = 7
+let myString = 'Some text'
+let myBoolean = true
+let myArray = ['apple', 'orange', 'pear']
+
+console.log(myNumber)
+console.log(myString)
+console.log(myBoolean)
+console.log(myArray)
