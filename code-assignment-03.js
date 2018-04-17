dc75708bf2a63abf0dcf4de80ee9dc678c06abfd
Hagan Doyle <hdoyle2@lsu.edu>
2018-04-17 18:10:40 -0500

    04.17.18 - 6:10 PM
    Code Assignment 3 - attempt

--- code-assignment-03.js
+++ code-assignment-03.js
@@ -1,9 +1,13 @@
-let myNumber = 7
-let myString = 'Some text'
-let myBoolean = true
-let myArray = ['apple', 'orange', 'pear']
+// assign values to a set of variables representing a project's metadata
+let projectId = 1
+let projectUrl = 'https://www.arcgis.com/apps/MapTour/index.html?appid=f58e1904bcbc486998b5debff9f55ecd'
+let projectTitle = 'A Brief Tour of Music City'
+let projectDesc = 'This is a quick Story Map showing a few music related locations in downtown Nashville, Tennessee.'
+let projectThumb = true
 
-console.log(myNumber)
-console.log(myString)
-console.log(myBoolean)
-console.log(myArray)
+// display messages in the browser's developer console, combining the values with text
+console.log('ID: ' + projectId)
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Description: ' + projectDesc)
+console.log('Has thumbnail? ' + projectThumb)
