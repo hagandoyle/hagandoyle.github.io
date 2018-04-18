8dda4f78add2721efd9d1992ef0d5114f52a52db
Hagan Doyle <hdoyle2@lsu.edu>
2018-04-17 23:07:21 -0500

    Code Assignment 5 - 04.17.18
    Code Assignment 5 - Trial 1

---
+++ js/code-assignment-05.js
@@ -0,0 +1,26 @@
+let project4 = {
+  id: 4,
+  title: 'Web App 2',
+  hasThumbnail: true
+}
+let project3 = {
+  id: 3,
+  title: 'Web App 1',
+  hasThumbnail: true
+}
+
+let projects = [
+  project4,
+  project3
+]
+
+for (let i = 4; i < projects.title; i++) {
+  // more code goes here
+}
+
+console.log('Element ' + i + ' title: Web App 2' + projects[i].title) // log the current project's title
+console.log('Midterm - Analysis Map' + projects[i].id + '.png') // build and log an image file name based on the project ID
+
+if (projects[i].hasThumbnail === true) {
+  console.log('Midterm - Analysis Map' + projects[i].id + '.png') // build and log an image file name based on the project ID
+}
