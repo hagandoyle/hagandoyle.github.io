a0ef6b644ccd526eba299bed23e3254a561e59c2
Hagan Doyle <hdoyle2@lsu.edu>
2018-05-01 18:38:39 -0500

    05.01.18 - Updates
    6:40 PM

--- code-assignment-04.js
+++ code-assignment-04.js
@@ -1,24 +1,24 @@
 // This is an array
 let projectArray = [
-  1,
-  'https://www.arcgis.com/apps/MapTour/index.html?appid=f58e1904bcbc486998b5debff9f55ecd',
-  'Tour of Nashville',
+  3,
+  'https://geog4046-hd.maps.arcgis.com/apps/webappviewer/index.html?id=43209437cfe04b49a50f86c825e6700f',
+  'BREC Park Locations',
   'A quick and simple story map looking at an array of music related locations in the downtown Nashville area.',
   true,
-  ['music', 'concerts', 'Nashville']
+  ['BREC', 'Parks', 'Louisiana']
 ]
 
 // This is an object. Each value is paired with a property.
 let projectObject = {
-  id: 1,
-  url: 'https://www.arcgis.com/apps/MapTour/index.html?appid=f58e1904bcbc486998b5debff9f55ecd',
-  title: 'Tour of Nashville',
+  id: 3,
+  url: 'https://geog4046-hd.maps.arcgis.com/apps/webappviewer/index.html?id=43209437cfe04b49a50f86c825e6700f',
+  title: 'BREC Park Locations',
   desc: 'A quick and simple story map looking at an array of music related locations in the downtown Nashville area.',
   thumb: true,
   keywords: [
-    'music',
-    'concerts',
-    'Nashville'
+    'BREC',
+    'Parks',
+    'Louisiana'
   ]
 }
 
