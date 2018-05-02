222a7e9122c349a4fa982e38c639fc55f14236a9
Hagan Doyle <hdoyle2@lsu.edu>
2018-05-01 22:29:24 -0500

    05.01.18 - WebMap 1
    try it out WebMap 1

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,2 +1,24 @@
 // let someName = L.map('BatonRouge-LA').setView([yourLat, yourLon], yourZoom)
-let BatonRougeLA = L.map('BatonRouge-LA').setView([51.505, -0.09], 13)
+let mymap = L.map('BatonRouge-LA').setView([51.505, -0.09], 13)
+
+l.tileLayer('http://leaflet-extras.github.io/leaflet-providers/preview/#filter=OpenStreetMap.BlackAndWhite')
+
+let marker = L.marker([51.5, -0.09]).addTo(mymap)
+
+let polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(mymap)
+
+polygon.bindPopup('St. Katharine\'s and Wapping')
+marker.bindPopup('London Bridge Station')
+
+function logCurrentCoordinates (event) {
+  console.log('You clicked the map at ' + event.latlng)
+}
+mymap.on('click', logCurrentCoordinates)
+
+mymap.on('click', function (event) {
+  console.log('You clicked the map at ' + event.latlng)
+})
