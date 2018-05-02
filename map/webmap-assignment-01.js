3a0929f8152cd9ef61ec941700afc9a0308e07e4
Hagan Doyle <hdoyle2@lsu.edu>
2018-05-02 11:51:18 -0500

    05.02.18 - Webmap 1
    Try it out. Webmap 1

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,20 +1,25 @@
 var L = require('leaflet')
 var l = require('basemap')
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+})
+
 // let someName = L.map('BatonRouge-LA').setView([yourLat, yourLon], yourZoom)
-let mymap = L.map('BatonRouge-LA').setView([51.505, -0.09], 13)
+let mymap = L.map('BatonRouge-LA').setView([30.414471, -91.178664], 18)
 
-l.tileLayer('http://leaflet-extras.github.io/leaflet-providers/preview/#filter=OpenStreetMap.BlackAndWhite')
+l.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(mymap)
 
-let marker = L.marker([51.5, -0.09]).addTo(mymap)
+let marker = L.marker([30.414471, -91.178664]).addTo(mymap)
 
 let polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
+  [30.413323, -91.176218],
+  [30.415840, -91.176368],
+  [30.415710, -91.178342],
+  [30.413231, -91.178171]
 ]).addTo(mymap)
 
-polygon.bindPopup('St. Katharine\'s and Wapping')
-marker.bindPopup('London Bridge Station')
+polygon.bindPopup('The Parade Ground\'s at Louisiana State University')
+marker.bindPopup('Memorial Bell Tower at Louisiana State University')
 
 function logCurrentCoordinates (event) {
   console.log('You clicked the map at ' + event.latlng)
